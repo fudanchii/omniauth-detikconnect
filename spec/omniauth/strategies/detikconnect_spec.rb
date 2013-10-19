e80c3abed361f00de4bcef9d4c896890252b221c
Nurahmadie <nurahmadie@gmail.com>
2013-10-19 15:33:42 +0700

    Remove github tests leftover

--- spec/omniauth/strategies/detikconnect_spec.rb
+++ spec/omniauth/strategies/detikconnect_spec.rb
@@ -5,21 +5,6 @@ describe OmniAuth::Strategies::DetikConnect do
   let(:parsed_response) { double('ParsedResponse') }
   let(:response) { double('Response', :parsed => parsed_response) }
 
-  let(:enterprise_site)          { 'https://some.other.site.com/api/v3' }
-  let(:enterprise_authorize_url) { 'https://some.other.site.com/login/oauth/authorize' }
-  let(:enterprise_token_url)     { 'https://some.other.site.com/login/oauth/access_token' }
-  let(:enterprise) do
-    OmniAuth::Strategies::DetikConnect.new('GITHUB_KEY', 'GITHUB_SECRET',
-        {
-            :client_options => {
-                :site => enterprise_site,
-                :authorize_url => enterprise_authorize_url,
-                :token_url => enterprise_token_url
-            }
-        }
-    )
-  end
-
   subject do
     OmniAuth::Strategies::DetikConnect.new({})
   end
@@ -39,20 +24,6 @@ describe OmniAuth::Strategies::DetikConnect do
 
     it 'should have correct token url' do
       subject.options.client_options.token_url.should eq('https://connect.detik.com/oauth/accessToken')
-    end
-
-    describe "should be overrideable" do
-      it "for site" do
-        enterprise.options.client_options.site.should eq(enterprise_site)
-      end
-
-      it "for authorize url" do
-        enterprise.options.client_options.authorize_url.should eq(enterprise_authorize_url)
-      end
-
-      it "for token url" do
-        enterprise.options.client_options.token_url.should eq(enterprise_token_url)
-      end
     end
   end
 
